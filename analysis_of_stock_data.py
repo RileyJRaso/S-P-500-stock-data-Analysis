165a91772c1f9e3cf8bf1760d6aa097eeb4799c8
RileyJRaso <riley.j.raso@gmail.com>
2021-12-07 22:31:30 -0800

    added data cleaning step to turn the date column into a datetime64 type in order to compare dates in later steps

--- analysis_of_stock_data.py
+++ analysis_of_stock_data.py
@@ -6,7 +6,8 @@ def get_Data(filepath):
     return(data)
 
 
-def clean_Data_only_close(data_uncleaned):
+def clean_Data(data_uncleaned):
+    data_uncleaned.loc[:,'date'] = pd.to_datetime(data_uncleaned.loc[:,'date'])
     cleaned_data = pd.DataFrame(data_uncleaned, columns=['date','close', 'Name'])
     return(cleaned_data)
 
@@ -15,6 +16,9 @@ def query_Data_for_stock(data_cleaned, stock):
     data_queried = data_cleaned.loc[data_cleaned.Name == stock,:]
     return data_queried
 
+def Select_years(Stock_data, starting_date, Ending_date):
+    return Stock_data
+
 def display_Data():
     pass
 
@@ -22,4 +26,5 @@ def display_Data():
 stock_data = get_Data(r'./Data/all_stocks_5yr.csv')
 cleaned_data = clean_Data_only_close(stock_data)
 queried_data = query_Data_for_stock(cleaned_data, 'AAL')
-print(queried_data)
+cleaned_queried_data = Select_years(queried_data, '2013-02-12', '2014-03-12')
+print(cleaned_queried_data.dtypes)
