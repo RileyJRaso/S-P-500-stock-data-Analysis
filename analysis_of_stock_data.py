5b3d4cf5d69b49d6b9e9c2725dd8eff7f2aeea29
RileyJRaso <riley.j.raso@gmail.com>
2021-12-07 21:28:18 -0800

    implemented a basic get_data() function

--- analysis_of_stock_data.py
+++ analysis_of_stock_data.py
@@ -1,10 +1,21 @@
 import pandas as pd
 import numpy as np
 
-def get_Data():
+def get_Data(filepath):
+    data = pd.read_csv(filepath)
+    return(data)
+
 
 def clean_Data():
+    pass
+
 
 def sort_Data():
+    pass
+
 
 def display_Data():
+    pass
+
+
+stack_data = get_Data(r'./Data/all_stocks_5yr.csv')
